72932196d8f26a4c73ca3a64fce641141d3d9e7b
ayushman <ayushmansingh2512@gmail.com>
2025-06-29 17:49:34 +0530

    added order to prejects complette

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -116,7 +116,7 @@ const ProjectsPage = async () => {
                             <svg className="mx-auto h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            <p className="mt-4">No projects found. Check back later!</p>
+                            <p className="mt-4">No project found Please check back letter</p>
                         </div>
                     </div>
                 )}
